50a0eb6f9c23bcf58a689c4ccf61fc621a9b0681
unknown <wanjikuclare82@gmail.com>
2023-10-13 11:27:19 +0300

    my calculator

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let num= document.getElementById("button");
+button=addEventListener("click",calcNumbers);
+
+    function calcNumbers(result){
+        result.preventDefault();
+        result.stopPropagation();
+		form.displayResult.value=form.displayResult.value+result;
+		
+
+
+}
