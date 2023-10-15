8b04e0dc61b0e0fede51aa7703826ed7f88f8989
unknown <wanjikuclare82@gmail.com>
2023-10-15 16:49:15 +0300

    changed the format

--- calculator.js
+++ calculator.js
@@ -1,11 +1,16 @@
-let num= document.getElementById("button");
-button=addEventListener("click",calcNumbers);
-
-    function calcNumbers(result){
-        result.preventDefault();
-        result.stopPropagation();
-		form.displayResult.value=form.displayResult.value+result;
-		
-
-
+ 
+function clr() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function displays the values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns the result
+function solve() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
 }
